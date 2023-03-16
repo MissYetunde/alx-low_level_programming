77a5c70275cae9906b38e564c295fa766d8c468d
MissYetunde <surajudeenyetunde@gmail.com>
2023-03-16 08:08:38 -0700

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
