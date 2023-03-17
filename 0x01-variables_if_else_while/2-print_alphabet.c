2791cf2fbad7f1f67e65b26d06b12a12cb38c21c
MissYetunde <surajudeenyetunde@gmail.com>
2023-03-17 08:46:14 -0700

    print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+			putchar(i);
+		putchar('\n');
+		return (0);
+}
