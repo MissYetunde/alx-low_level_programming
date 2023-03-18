5795009d472094b508cd22bf6f60d6b48262078a
MissYetunde <surajudeenyetunde@gmail.com>
2023-03-17 21:02:29 -0700

    print comb 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+int i, j;
+
+for (i = 0; i <= 99; i++)
+{
+for (j = i; j <= 99; j++)
+{
+int a = i / 10;
+int b = i % 10;
+int c = j / 10;
+int d = j % 10;
+putchar(a + '0');
+putchar(b + '0');
+putchar(' ');
+putchar(c + '0');
+putchar(d + '0');
+if (i != 99 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+return (0);
+}
