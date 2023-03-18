cfa4cd8c9a5298a4c4398cfab54415e0b844a81f
MissYetunde <surajudeenyetunde@gmail.com>
2023-03-17 20:19:23 -0700

    prints comb 3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,18 +13,17 @@ int j;
 
 for (i = 0 ; i < 10 ; i++)
 {
-
 for (j = 1 ; j < 10 ; j++)
 {
 if (i < j && i != j)
 {
 putchar(i + '0');
 putchar(j + '0');
-}
 if (i + j != 17)
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
