28011bdb185a3075eebfce042cacdde4b9dd838d
MissYetunde <surajudeenyetunde@gmail.com>
2023-03-21 13:01:49 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - make alphabet x10 times
+ *Return: 0 success
+ */
+
+void print_alphabet_x10(void)
+{
+char c;
+int i = 0;
+
+while (i <= 0)
+{
+for (c = 'a' ; c <= 'a' ; c++)
+{
+_putchar(c);
+} 
+_putchar('\n');
+i++;
+}
+}
