500ced397e9d159504e6ec2b7a7934c38f89dd69
MissYetunde <surajudeenyetunde@gmail.com>
2023-03-22 05:43:03 -0700

    fibonaci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,9 +6,10 @@
  */
 int main(void)
 {
+
 int count;
 unsigned long fib1 = 0, fib2 = 1, sum;
-unsigned long fib1_half1, fib1_half2, fib2_half1,  fib2_half2;
+unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 unsigned long half1, half2;
 
 for (count = 0 ; count < 92; count++)
@@ -29,7 +30,7 @@ half2 = fib1_half2 + fib2_half2;
 if (fib1_half2 + fib2_half2 > 9999999999)
 {
 half1 += 1;
-half2 %= 1000000000;
+half2 %= 10000000000;
 }
 printf("%lu%lu", half1, half2);
 if (count != 98)
