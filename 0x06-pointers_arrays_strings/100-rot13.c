b7282ddeb633b95eb1737fffc326bc520c503751
MissYetunde <surajudeenyetunde@gmail.com>
2023-03-30 12:22:48 -0700

    100 rots

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * rot13 - Encodes a string using rot13.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *rot13(char *str)
+{
+	char *ptr = str;
+
+while (*ptr != '\0')
+{
+	if ((*ptr >= 'a' && *ptr <= 'm') || (*ptr >= 'A' && *ptr <= 'M'))
+	{
+	*ptr += 13;
+	}
+	else if ((*ptr >= 'n' && *ptr <= 'z') || (*ptr >= 'N' && *ptr <= 'Z'))
+	{
+	*ptr -= 13;
+	}
+	ptr++;
+	}
+	return (str);
+}
