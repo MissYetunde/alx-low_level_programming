2cc151c76f4569bcc2582326b2dcb94f8c92118c
MissYetunde <surajudeenyetunde@gmail.com>
2023-05-04 05:50:09 -0700

    binary to unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,8 +5,9 @@
  *
  * @b: Pointer to a string of 0 and 1 chars
  *
- * Return: The converted number, or 0 if there is one or more chars in the string
- *         b that is not 0 or 1
+ * Return: The converted number, or 0 if there is one or
+ * more chars in the string
+ * b that is not 0 or 1
  */
 unsigned int binary_to_uint(const char *b)
 {
